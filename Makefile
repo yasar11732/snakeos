9d3869ef3cf98f3783ba92bdcd3781dc74a1d206
Yaşar Arabacı <yasar11732@gmail.com>
2018-09-21 23:08:47 +0000

    BIOS printing

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 
-boot.iso: build/boot.bin
+boot.iso: deploy/boot.bin
 	mkisofs -b boot.bin -hide boot.bin -iso-level 3 -no-emul-boot -o boot.iso deploy/
 
 deploy/boot.bin: build/boot.o boot.ld
-	ld -Tboot.ld -nostartfiles -nostdlib boot.o -o build/boot.bin
+	ld -Tboot.ld -nostartfiles -nostdlib boot.o -o deploy/boot.bin
 
 build/boot.o: boot.s
 	as boot.s -o boot.o
--- boot.s
+++ boot.s
@@ -1,4 +1,29 @@
 .section .boot
 .code16
+
+# setup data segment pointer
+mov $0x7D0, %ax
+mov %ax, %ds
+
+# set source index to point to hello string
+mov $(hello-.data), %si
+call bios_print
+
 hang:
+    hlt
     jmp hang
+
+# print all characters
+bios_print:
+    lodsb
+    or %al, %al
+    jz return
+    mov $0x0E, %ah
+    int $0x10
+    jmp bios_print
+return:
+    ret
+
+.section .data
+hello:
+    .ascii "Hello World!\0"

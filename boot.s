c0c884debcd8bea0a3911e3e4e8c2a8d7ba639eb
Yaşar Arabacı <yasar11732@gmail.com>
2018-09-24 19:59:22 +0000

    read keyboard

--- boot.s
+++ boot.s
@@ -7,7 +7,8 @@ mov %ax, %ds
 
 # set source index to point to hello string
 mov $(hello-.data), %si
-call bios_print
+mov $0, %di
+call write_to_screen
 
 hang:
     hlt
@@ -21,6 +22,61 @@ bios_print:
     mov $0x0E, %ah
     int $0x10
     jmp bios_print
+
+write_to_screen:
+    mov $0, %ah
+    int $0x16 # wait keypress
+    
+    cmp 0x08, %al
+    je enter_pressed
+    
+    mov $0x0E, %ah
+    int $0x10 # output keypress
+    jmp write_to_screen
+
+enter_pressed:
+    mov $0x0E,%ah
+    mov $0x0D,%al
+    int $0x10
+    mov $0x0A,%al
+    int $0x10
+    jmp write_to_screen
+
+# vga print
+vga_print:
+    mov $15, %ah
+    movb %ds:(%si), %al
+    inc %si
+    sub $0, %al
+    jz return
+    push ycoord
+    push xcoord
+    call terminal_putcharat
+    add $4,%sp
+    incw xcoord
+    mov xcoord, %ax
+    sub $VGA_WIDTH, %ax
+    jnz vga_print
+    movw $0, xcoord
+    incw ycoord
+    jmp vga_print
+
+
+terminal_putcharat:
+    push %bp
+    mov %sp, %bp
+
+    push %di
+    
+    mov 4(%bp), %di
+    shlw $1, %di
+    
+    movw %ax,%es:(%di)
+
+    pop %di
+    pop %bp
+    ret
+
 return:
     ret
 

4666dd856790cc4e71a4c6419b9004aaa5a888da
Yaşar Arabacı <yasar11732@gmail.com>
2018-09-30 16:30:13 +0000

    Direction Switching Works

--- snake.s
+++ snake.s
@@ -3,7 +3,7 @@
 
 # calling convention: pass arguments and return values using registers
 # all registers except %ax is calee saved, %ax is caller saved
-
+start:
 setup:
     cli
     # setup stack segment
@@ -16,7 +16,8 @@ setup:
 
     # setup video memory
     mov $0xB800, %ax
-    mov %ax, %gs
+    mov %ax, %es
+
     sti
 
 init_random_value:
@@ -31,49 +32,68 @@ register_interrupt_handlers:
     movw $keyboard_handler,(36)
     movw $0,(38)
     sti
-
-/*
+    mov $0,%di
 busy_loop:
+    /*
     mov timer_ticks,%ax
     add $1,%ax
 _wait:
-    mov timer_ticks,%bx
-    cmp %ax,%bx
+    mov timer_ticks,%cx
+    cmp %ax,%cx
     jge stop_waiting
     hlt
     jmp _wait
 stop_waiting:
-    call get_random
-    call printd
-
-    mov $0x0E,%ah
-    mov $0xD,%al
-    int $0x10
-    mov $0xA,%al
-    int $0x10
-    jmp busy_loop
 */
+    movb direction,%al
+    xor %ah,%ah
+    mov %ax,%di
+    mov $direction_symbols,%bx
+    mov (%bx,%di),%al
+    mov $0x15,%ah
+    mov %ax,%es:2
+    jmp busy_loop
+
+irq_return:
+    mov $0x20,%al
+    out %al,$0x20
+    popa
+    iret
 
 timer_handler:
     pusha
     incw timer_ticks
-
-    mov $0x20,%al
-    out %al,$0x20
-    popa
-    iret
+    jmp irq_return
 
 keyboard_handler:
     pusha
     in $0x60,%al
-    mov %al,scancode
-    mov $0,%ah
-    call printd
-    call printnewline
-    mov $0x20,%al
-    out %al,$0x20
-    popa
-    iret
+
+    mov direction,%bx
+    test $1,%bl
+    jz vert
+horiz:
+    cmp $0x11,%al
+    jne horiz2
+    movb $0,direction
+    jmp irq_return
+horiz2:
+    cmp $0x1F,%al
+    jne irq_return
+    movb $0x2,direction
+    jmp irq_return
+vert:
+    cmp $0x20,%al
+    jne vert2
+    movb $0x1,direction
+    jmp irq_return
+vert2:
+    cmp $0x1E,%al
+    jne irq_return
+    movb $0x3,direction
+    jmp irq_return
+
+
 
 /* Returns 16bit pseudo-random number in ax */
 .global get_random
@@ -96,21 +116,52 @@ printd:
     push %dx
     push $10 # end of digits in stack
     mov $10,%bx
-divide_loop:
+LOC0:
     mov $0,%dx
     div %bx
     push %dx
     or %ax,%ax
-    jnz divide_loop
-print_loop:
+    jnz LOC0
+LOC1:
     pop %ax
     cmp $10,%ax
-    je printd_quit
+    je LOC2
     mov $0x0E,%ah
     add $48,%al
     int $0x10
-    jmp print_loop
-printd_quit:
+    jmp LOC1
+LOC2:
+    pop %dx
+    pop %bx
+    ret
+
+# print hex representation of a %ax
+.global printh
+.type printh,@function
+printh:
+    push %bx
+    push %dx
+    mov $0x10,%bx
+    push %bx
+LOC3:
+    mov $0,%dx
+    div %bx
+    push %dx
+    or %ax,%ax
+    jnz LOC3
+LOC4:
+    pop %ax
+    cmp %bx,%ax
+    je LOC5
+    mov $0x0E,%ah
+    cmp $10,%al
+    jl LOC6
+    add $7,%al
+LOC6:
+    add $48,%al
+    int $0x10
+    jmp LOC4
+LOC5:
     pop %dx
     pop %bx
     ret
@@ -127,11 +178,12 @@ random:
     .2byte 0x0
 timer_ticks:
     .2byte 0x0
-scancode:
-    .2byte 0x0
-cursorx:
-    .byte 0x0
-cursory:
-    .byte 0x0
-terminalcolor:
-    .2byte 0x15
+
+.align 16
+direction_symbols:
+    .byte 24
+    .byte 26
+    .byte 25
+    .byte 27
+direction:
+    .byte 0x1

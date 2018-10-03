bebf93953a4626019a7c5e3cf795907c7f384132
Yaşar Arabacı <yasar11732@gmail.com>
2018-10-03 23:06:20 +0300

    Compress Code Further

--- snake.s
+++ snake.s
@@ -21,10 +21,6 @@ setup:
     mov %ax, %ss
     mov %ax, %ds
 
-    # keyboard handler
-    movw $keyboard_handler,(36)
-    mov %ax,(38)
-
     sti
 
 init_random_value:
@@ -32,37 +28,63 @@ init_random_value:
     mov %dx,random
    
    
-copy_snake_prepare:
+init_game:
     # copy initial state of snake into 0x7E00
     mov $0x7E00,%bx
-    mov $0x0D27,%ax
+    mov $0x876,%ax
     mov $4,%cx
     
-copy_snake:
+init_snake:
     mov %ax,(%bx)
-    inc %ax
-    add $2,%bx
-    loop copy_snake
-
-copy_snake_finalize:
+    dec %ax
+    dec %ax
+    inc %bx
+    inc %bx
+    loop init_snake
     movw $0xFFFF,(%bx)
     /* set callback for loop snake */
     mov $draw_snake,%bx
+    mov $2,%dx # current direction
 
-busy_loop:
-
+game_loop:
+    # wait 18 ints
     mov $18,%cx
-    call sleep
-
-    mov $0x0,%cx
-    call loop_snake
+sleep:
+    hlt
+    loop sleep
     
-    call mov_snake
+    # handle a keypress
+    mov $1,%ah
+    int $0x16
+    jz keypress_done # keypress is not available
+    mov $0,%ah # get keypress
+    int $0x16
+    test $0xF,%dx # this is 0 if we are moving vertically
+    jz moving_vert1
+moving_horiz1:
+    cmp $'w',%al
+    jne moving_horiz2
+    mov $-0xA0,%dx
+moving_horiz2:
+    cmp $'s',%al
+    jne keypress_done
+    mov $0xA0,%dx
+moving_vert1:
+    cmp $'d',%al
+    jne moving_vert2
+    mov $2,%dx
+moving_vert2:
+    cmp $'a',%al
+    jne keypress_done
+    mov $-2,%dx
 
+keypress_done:
+    # cx is zero here because of sleep loop
+    call loop_snake
+    call mov_snake
     mov $0x0f09,%cx
     call loop_snake
-
-    jmp busy_loop
+    jmp game_loop
 
 /*  foreach part of the snake, call bx with position in ax
     cx and dx is preserved before calling bx, so they can
@@ -83,8 +105,7 @@ loop_snake1:
     pop %ds
     ret
 
-draw_snake:    
-    call snake_to_screen
+draw_snake:
     mov %ax,%di
     mov %cx,%es:(%di)
     ret
@@ -127,82 +148,13 @@ mov_snake1:
     # ! important ! fix ds for these references to work
     # ! important ! don't fix es yet, for stos to work
     pop %ds
-    mov nextdirection,%bx
-    mov %bx,direction
-    
-    # calculate new head location based on direction
-    add %bl,%al 
-    add %bh,%ah
+    add %dx,%ax
     stosw
     cld
     pop %es
     popa
     ret
 
-/* put ticks to sleep in cx,
- * this assumes other interrupts except for timer is negligible (for simplicity)
- * cx is trashed
- */
-sleep:
-    hlt
-    loop sleep
-    ret
-
-/* Convert snake coordinates into video memory index
-    in: al -> snake x
-    in: ah -> snake y
-    out: ax -> index in video memory
-*/
-snake_to_screen:
-    push %bx
-    push %cx
-
-    mov $80,%cx
-    xor %bx,%bx
-    
-    mov %al,%bl
-    shr $8,%ax
-    mul %cx
-    add %bx,%ax
-    shl $1,%ax
-
-    pop %cx
-    pop %bx
-    ret
-
-irq_return:
-    mov $0x20,%al
-    out %al,$0x20
-    popa
-    iret
-
-keyboard_handler:
-    pusha
-    in $0x60,%al
-
-    mov direction,%bx
-    or %bl,%bl # if low byte of direction is zero, we are movin vertically
-    jz vert
-horiz:
-    cmp $0x11,%al # w pressed
-    jne horiz2
-    movw $0xff00,nextdirection
-    jmp irq_return
-horiz2:
-    cmp $0x1F,%al # s pressed
-    jne irq_return
-    movw $0x0100,nextdirection
-    jmp irq_return
-vert:
-    cmp $0x20,%al # d pressed
-    jne vert2
-    movw $0x0001,nextdirection
-    jmp irq_return
-vert2:
-    cmp $0x1E,%al # a pressed
-    jne irq_return
-    movw $0x00ff,nextdirection
-    jmp irq_return
 
 /* Returns 16bit pseudo-random number in ax */
 .global get_random
@@ -219,7 +171,3 @@ get_random:
 .align 2
 random:
     .2byte 0x0
-direction:
-    .2byte 0x0001
-nextdirection:
-    .2byte 0x0001
